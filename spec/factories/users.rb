0afa70355d4a3a1e4b07110e115134313e2290d6
osman afridi <osmanisalso@gmail.com>
2014-12-23 15:33:19 -0800

    Updated users factory. Added tests for the instance methods in the User model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryGirl.define do
   factory :user do
     first_name Faker::Name.name 
     email Faker::Internet.email
+    address_line_1 "59 Yale Street"
+    city "SF"
+    zipcode 94134
     password Faker::Internet.password(8)
+    latitude 37
+    longitude 122
   end
 
 end
