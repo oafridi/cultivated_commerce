2bcfca574905bf62f5b32c827512bff87075ce02
osman afridi <osmanisalso@gmail.com>
2014-12-23 15:00:39 -0800

    added more user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,19 +37,15 @@ describe User, :type => :model do
 
       it 'is invalid when longer than 128 characters' do
         user.password = 'x' * 129
-        expect(user.errors_on(:password)).to include('is too long (maximum is 128 characters)')
+        user.valid?
+        expect(user.errors[:password]).to include('is too long (maximum is 128 characters)')
       end
 
       it 'is invalid when shorter than 8 characters' do
         user.password = 'x' * 4
-        expect(user.errors_on(:password)).to include('is too short (minimum is 8 characters)')
+        user.valid?
+        expect(user.errors[:password]).to include('is too short (minimum is 8 characters)')
       end
-
-      # it 'ensures the confirmation matches' do
-      #   user.password = 'test1234'
-      #   user.password_confirmation = 'not correct'
-      #   expect(user.errors_on(:password)).to include('this must match confirmation')
-      # end
     end
   end
 
