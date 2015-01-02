e3194bdced98b84373fc46d41e3b99c431cd3558
osman afridi <osmanisalso@gmail.com>
2015-01-02 12:31:48 -0800

    render flash devise messages in layout

--- spec/features/visitor_logs_in_spec.rb
+++ spec/features/visitor_logs_in_spec.rb
@@ -7,6 +7,12 @@ feature "user logs in" do
     expect(page).not_to have_content("Sign in")
     expect(page).to have_content(user.first_name)
     expect(page).to have_content("logout")
+  end
+
+  scenario "with invalid email or password" do
+    user = build(:user)
+    sign_in_with user
+    expect(page).to have_content("Invalid email or password")
   end  
 
 end
