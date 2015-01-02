7b29da00605233a6c45147ad321522fcec6ec987
osman afridi <osmanisalso@gmail.com>
2015-01-02 15:21:39 -0800

    add feature test for signup with invalid credentials

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -4,22 +4,26 @@ feature 'Visitor signs up' do
   let(:user) { build(:user) }
 
   scenario "with valid email and password" do
-    
-    visit root_path
-    click_link 'Sign up'      
-
-    fill_in 'user_first_name', with: user.first_name
-    fill_in 'user_last_name', with: user.last_name
-    find('#js-email-signup').set(user.email)
-    fill_in 'user_address_line_1', with: user.address_line_1
-    fill_in 'user_city', with: user.city
-    fill_in 'user_zipcode', with: user.zipcode
-    find('#js-password-signup').set(user.password)
-    find('#js-password-confirmation-signup').set(user.password)
-    click_button 'Sign up'
+    sign_up_with(user)    
     expect(page).not_to have_content("Sign in")
     expect(page).to have_content("logout")
     expect(page).to have_content("logout")
+  end
+
+  scenario "with invalid email" do
+    user.email = ""
+    sign_up_with(user)
+    expect(page).to have_content("Email can't be blank")
+
+    user.email = "oooo@oooo"
+    sign_up_with(user)
+    expect(page).to have_content("Email is invalid")
+  end
+
+  scenario "with invalid password" do
+    user.password = ""
+    sign_up_with(user)
+    expect(page).to have_content("Password can't be blank")
   end
 
 end
