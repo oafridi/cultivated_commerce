7d3108a88b1b489797888076ebb5a3496488abab
osman afridi <osmanisalso@gmail.com>
2015-01-03 14:27:58 -0800

    add model tests for Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ActiveRecord::Base
-  has_many :listings  
+  has_many :listings
+  
+  validates :kind, 
+    presence: true,
+    format: { with: /\A[a-zA-Z\s]*\z/, message: "can only contain letters" }
+  validates :category, 
+    presence: true    
 end
