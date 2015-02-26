e8e9d8e1842f9e56bb55768d18410e0d53707480
osman afridi <osmanisalso@gmail.com>
2015-02-26 12:51:21 -0800

    tidy seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,37 +4,23 @@ v1 = User.create(first_name:'Bob', last_name:'Marley', user_name:'chrisb',
                  private_contact: true, user_img:'chris.jpg', password:'12345678', 
                  password_confirmation:'12345678')
 
-v1 = User.create(first_name:'Bob', last_name:'Marley', user_name:'chrisb', 
-                 email:'bobmarley@gmail.com', address_line_1:'59 yale street', 
+v2 = User.create(first_name:'Bob', last_name:'Marley', user_name:'chrisb', 
+                 email:'bobmarlet@gmail.com', address_line_1:'59 yale street', 
                  zipcode:'94134', about:'I play sax', phone:'555-717-5555', 
                  private_contact: true, user_img:'chris.jpg', password:'12345678', 
                  password_confirmation:'12345678')
 
-v1 = User.create(first_name:'Bob', last_name:'Marley', user_name:'chrisb', 
-                 email:'bobmarley@gmail.com', address_line_1:'59 yale street', 
+v3 = User.create(first_name:'Bob', last_name:'Marley', user_name:'chrisb', 
+                 email:'bobmarler@gmail.com', address_line_1:'59 yale street', 
                  zipcode:'94134', about:'I play sax', phone:'555-717-5555', 
                  private_contact: true, user_img:'chris.jpg', password:'12345678', 
                  password_confirmation:'12345678')
 
-v1 = User.create(first_name:'Bob', last_name:'Marley', user_name:'chrisb', 
-                 email:'bobmarley@gmail.com', address_line_1:'59 yale street', 
+v4 = User.create(first_name:'Bob', last_name:'Marley', user_name:'chrisb', 
+                 email:'bobmarlee@gmail.com', address_line_1:'59 yale street', 
                  zipcode:'94134', about:'I play sax', phone:'555-717-5555', 
                  private_contact: true, user_img:'chris.jpg', password:'12345678', 
                  password_confirmation:'12345678')
-
-users = [v1, v2, v3, v4]
-
-case Rails.env
-when "development"
-  10.times do
-    User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, 
-                user_name:Faker::Internet.user_name, email: Faker::Internet.email, 
-                address_line_1: Faker::Address.street_name, zipcode: Faker::Address.zip_code, 
-                about: Faker::Lorem.sentence, phone:Faker::PhoneNumber.phone_number, 
-                private_contact: false, user_img: "user2" ,password:'12345678',
-                password_confirmation:'12345678')
-  end
-end
 
 produce = Item.create(kind:'Produce', category:'i-produce.svg')
 eggsdairy = Item.create(kind:'Eggs and Dairy', category:'i-eggs_dairy.svg')
@@ -43,22 +29,38 @@ bakedgoods = Item.create(kind:'Baked Goods', category:'i-bakery.svg')
 pantry = Item.create(kind:'Pantry', category:'i-pantry.svg')
 herbsspices = Item.create(kind:'Herbs and Spices', category:'i-herbs_spices.svg')
 
-v1.listings.create(title:'Eggs', item_id: eggsdairy.id, description:'Eggs from my city chickens',desired:'apple',quantity:'3 dozen',image:"egg3.jpg",active:true)
-v1.listings.create(title:'Eggs', item_id: eggsdairy.id, description:'A lot of extra eggs lorem ipsum',desired:'apple',quantity:'3 dozen', active:true, image: "egg1.jpg")
-v1.listings.create(title:'Eggs', description:'Fresh eggs from our farm', item_id: eggsdairy.id, desired:'apple',quantity:'3 dozen',active:true, image: "egg2.jpg")
-v1.listings.create(title:'Oranges', item_id: produce.id, description:'From my two orange trees. Large size.',desired:'Lemons',quantity:'3 dozen',image:"orange2.jpg",active:true)
-v2.listings.create(title:'Meyer Lemons', item_id: produce.id, description:'So many lemons for trade.',desired:'Avocado',quantity:'100',image:"lemon1.jpg",active:true)
-v2.listings.create(title:'Meyer Lemons', item_id: produce.id,description:'So many lemons for trade.',desired:'Avocado',quantity:'100',image:"lemon2.jpg",active:true)
-v2.listings.create(title:'Meyer Lemons', item_id: produce.id, description:'So many lemons for trade.',desired:'Avocado',quantity:'100',image:"lemon3.jpg",active:true)
-v2.listings.create(title:'Meyer Lemons', item_id: produce.id, description:'So many lemons for trade.',desired:'Avocado',quantity:'100',image:"lemon4.jpg",active:true)
-v3.listings.create(title:'Persimmons', item_id: produce.id, description:'From my tree',desired:'Eggs',quantity:'30',image:"persimmons2.jpg",active:true)
-v3.listings.create(title:'Heirloom Tomatoes', item_id: produce.id, description:'I have as many as I need. The rest are for trade.',desired:'Eggs',quantity:'50',image:"tomatoes1.jpg",active:true)
-v3.listings.create(title:'Vine Tomatoes', item_id: produce.id, description:'Extra large. Extra tasty.',desired:'Avocado',quantity:'20',image:"tomatoes2.jpg",active:true)
-v4.listings.create(title:'Honey', item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',desired:'Oranges',quantity:'30 jars',image:"honey1.jpg",active:true)
-v4.listings.create(title:'Honey', item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',desired:'Oranges',quantity:'30 jars',image:"honey2.jpg",active:true)
-v4.listings.create(title:'Honey', item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',desired:'Oranges',quantity:'30 jars',image:"honey4.jpg",active:true)
-v4.listings.create(title:'Honey',item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',desired:'Oranges',quantity:'30 jars',image:"honey6.jpg",active:true)
-v4.listings.create(title:'Avocados', item_id: produce.id, description:'Haas avocados',desired:'Eggs',quantity:'40',image:"avocado.jpg",active:true)
+v1.listings.create(title:'Eggs', item_id: eggsdairy.id, description:'Eggs from my city chickens', 
+                    desired:'apple',quantity:'3 dozen',image:"egg3.jpg",active:true)
+v1.listings.create(title:'Eggs', item_id: eggsdairy.id, description:'A lot of extra eggs lorem ipsum',
+    desired:'apple',quantity:'3 dozen', active:true, image: "egg1.jpg")
+v1.listings.create(title:'Eggs', description:'Fresh eggs from our farm', item_id: eggsdairy.id, 
+    desired:'apple',quantity:'3 dozen',active:true, image: "egg2.jpg")
+v1.listings.create(title:'Oranges', item_id: produce.id, description:'From my two orange trees. Large size.',
+    desired:'Lemons',quantity:'3 dozen',image:"orange2.jpg",active:true)
+v2.listings.create(title:'Meyer Lemons', item_id: produce.id, description:'So many lemons for trade.',
+    desired:'Avocado',quantity:'100',image:"lemon1.jpg",active:true)
+v2.listings.create(title:'Meyer Lemons', item_id: produce.id,description:'So many lemons for trade.',
+    desired:'Avocado',quantity:'100',image:"lemon2.jpg",active:true)
+v2.listings.create(title:'Meyer Lemons', item_id: produce.id, description:'So many lemons for trade.',
+    desired:'Avocado',quantity:'100',image:"lemon3.jpg",active:true)
+v2.listings.create(title:'Meyer Lemons', item_id: produce.id, description:'So many lemons for trade.',
+    desired:'Avocado',quantity:'100',image:"lemon4.jpg",active:true)
+v3.listings.create(title:'Persimmons', item_id: produce.id, description:'From my tree',
+    desired:'Eggs',quantity:'30',image:"persimmons2.jpg",active:true)
+v3.listings.create(title:'Heirloom Tomatoes', item_id: produce.id, description:'I have as many as I need. The rest are for trade.',
+    desired:'Eggs',quantity:'50',image:"tomatoes1.jpg",active:true)
+v3.listings.create(title:'Vine Tomatoes', item_id: produce.id, description:'Extra large. Extra tasty.',
+    desired:'Avocado',quantity:'20',image:"tomatoes2.jpg",active:true)
+v4.listings.create(title:'Honey', item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',
+    desired:'Oranges',quantity:'30 jars',image:"honey1.jpg",active:true)
+v4.listings.create(title:'Honey', item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',
+    desired:'Oranges',quantity:'30 jars',image:"honey2.jpg",active:true)
+v4.listings.create(title:'Honey', item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',
+    desired:'Oranges',quantity:'30 jars',image:"honey4.jpg",active:true)
+v4.listings.create(title:'Honey',item_id: pantry.id, description:'I have a small behive up north and I bring back honey to trade.',
+    desired:'Oranges',quantity:'30 jars',image:"honey6.jpg",active:true)
+v4.listings.create(title:'Avocados', item_id: produce.id, description:'Haas avocados',
+    desired:'Eggs',quantity:'40',image:"avocado.jpg",active:true)
 
 v1.events.create(address_line_1:'590 Dolores St', city: 'San Francisco',
                  zipcode:'94110', date:Faker::Date.forward(30), time:'9am - 1pm', 
