1f16965c8aae5e0863e092d0c5040310fc6c3b0e
osman afridi <osmanisalso@gmail.com>
2016-07-04 09:56:30 -0700

    Extract method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,13 @@ class PagesController < ApplicationController
     if !params[:search]
       @events = Event.all.uniq
       @listings = Listing.all.uniq
-      @hash = build_hash
+      @markers = build_hash
     elsif !user_signed_in?
       @listings = Listing.search(params[:search])
       users = @listings.map { |listing| listing.user }
       @events = users.map { |user| user.events }.uniq.flatten
 
-      @hash = build_hash
+      @markers = build_hash
     else
       counter = 0
       @listings = Listing.search(params[:search])
@@ -20,17 +20,10 @@ class PagesController < ApplicationController
       events_unsorted = users.map { |u| u.events }.uniq.flatten
       @events = sort_events(events_unsorted)
 
-      @hash = Gmaps4rails.build_markers(@events) do |event, marker|
+      @markers = Gmaps4rails.build_markers(@events) do |event, marker|
         counter +=1
         event.counter = counter
-        if event.hosts.first == current_user
-          color = 'f7b100'
-        elsif event.participants.include?(current_user)
-          color = 'd34f4f'
-        else
-          color = 'D0D0D0'
-        end
-
+        color = get_color(event)
         marker.lat event.latitude
         marker.lng event.longitude
         marker.infowindow render_to_string(:partial => '/events/show', :locals => {:object => event})
@@ -41,7 +34,7 @@ class PagesController < ApplicationController
                            :height => 32
                        })
       end
-      @hash.push(
+      @markers.push(
           {:lat => current_user.latitude,
            :lng => current_user.longitude,
            :infowindow =>
@@ -49,6 +42,17 @@ class PagesController < ApplicationController
            :picture => {:url => 'https://chart.googleapis.com/chart?chst=d_map_xpin_icon&chld=pin_star%7Chome%7C00FFFF%7CFF0000', :width => 32, :height => 32}}
       )
     end
+  end
+
+  def get_color(event)
+    if event.hosts.first == current_user
+      color = 'f7b100'
+    elsif event.participants.include?(current_user)
+      color = 'd34f4f'
+    else
+      color = 'D0D0D0'
+    end
+    color
   end
 
   private
